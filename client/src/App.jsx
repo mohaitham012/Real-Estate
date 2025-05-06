77e034d93e8ed2417db486bbcc1845c7c1d543f1
Mohammed Haitham <mohamedhaithame320@gmail.com>
2025-05-06 22:52:25 +0200

    finishing properties

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,37 @@
+import { Suspense } from "react";
 import "./App.css";
-import Companies from "./components/Companies/Companies";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import GetStarted from "./components/GetStarted/GetStarted";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Residencies from "./components/Residencies/Residencies";
-import Value from "./components/Value/Value";
+import Website from "./pages/Website";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Properties from "./pages/Properties/Properties";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ToastContainer } from "react-toastify";
+import Property from "./pages/property/Property";
+
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <div>
-        <div className="white-gradient" />
-        <Header />
-        <Hero />
-      </div>
-      <Companies />
-      <Residencies/>
-      <Value/>
-      <Contact/>
-      <GetStarted/>
-      <Footer/>
-    </div>
+    <>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}/>
+        <Routes>
+        <Route element={<Layout />}>          
+          <Route path="/" element={<Website />} />
+          <Route path="/properties">
+            <Route index element={<Properties />}/>
+            <Route path=":propertyId" element={<Property />}/>
+          </Route>
+        </Route>  
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false}/>
+      </QueryClientProvider>  
+    </>
   );
 }
 
